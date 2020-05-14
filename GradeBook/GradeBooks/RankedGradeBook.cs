dfd4b63395414d6d92fa178d9a08aab62c890408
Mike <venteh@gmail.com>
2020-05-15 00:32:16 +0100

    Module 3
    Override RankedGradebooks GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,6 +11,49 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int noOfStudents = Students.Count;
+            if (noOfStudents < 5)
+            {
+                throw new InvalidOperationException("Need at least 5 students to calculate ranked grade");
+            }
+
+            //Get a list of average grades
+            var averageGrades = new List<double>();
+            foreach (Student student in Students)
+            {
+                averageGrades.Add(student.AverageGrade);
+            }
+
+            //Sort the average grades into descending order
+            averageGrades.Sort();
+            averageGrades.Reverse();
+            
+            //Determine where in the ordered list this average grade would sit
+            int gradePosition = 0;
+            for(; gradePosition < averageGrades.Count; gradePosition++)
+            {
+                if (averageGrade >= averageGrades[gradePosition])
+                {
+                    break;
+                }
+            }
+
+            double gradeBoundary = noOfStudents / 5;
+            if (gradePosition < gradeBoundary)
+                return 'A';
+            else if (gradePosition < gradeBoundary * 2)
+                return 'B';
+            else if (gradePosition < gradeBoundary * 3)
+                return 'C';
+            else if (gradePosition < gradeBoundary * 4)
+                return 'D';
+            else
+                return 'F';
+
         }
     }
 }
